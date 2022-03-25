c1aed361233a4eb09bb043ef51134f6811645134
洪啟睿 <y6602016@gmail.com>
2022-03-24 20:51:36 -0400

    BST insertion

--- HW3/CS590_HW03_code/bs_tree.cpp
+++ HW3/CS590_HW03_code/bs_tree.cpp
@@ -28,6 +28,50 @@ void bs_tree::insert(int key, bs_tree_i_info &t_info)
 
 void bs_tree::insert(bs_tree_node *z, bs_tree_i_info &t_info)
 {
+  // use two pointers: x and y to run the insert algo
+  bs_tree_node *x;
+  bs_tree_node *y;
+
+  // initialize y = NULL and x = root
+  y = NULL;
+  x = T_root;
+
+  // traverse x to the correct position to insert z
+  while (x != NULL)
+  {
+    y = x;
+    if (z->key < x->key)
+    { // z val < x val, go left
+      x = x->left;
+    }
+    else if (z->key > x->key)
+    { // z val > x val, go left
+      x = x->right;
+    }
+    else
+    { // z val = x val, return
+      return;
+    }
+  }
+
+  // now x = NULL and y is at the position of being the parent of z
+  z->p = y;
+
+  if (y == NULL)
+  { // if y = NULL, it's an empty tree, set z as the root
+    T_root = z;
+  }
+  else
+  {
+    if (z->key < y->key)
+    { // if z val < y val, then z is y's left child
+      y->left = z;
+    }
+    else
+    {
+      y->right = z; // otherwise z is y's right child
+    }
+  }
 }
 
 // TODO: modified inorder tree walk method to save the

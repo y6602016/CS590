2cbd148e57714633bc76fa516d7759cfff364c43
洪啟睿 <y6602016@gmail.com>
2022-04-08 14:52:06 -0400

    finish coding

--- HW4/cs590_hw4_code/dynprog.cpp
+++ HW4/cs590_hw4_code/dynprog.cpp
@@ -6,38 +6,228 @@
 
 using namespace std;
 
-
 /*
  * Bottom up implementation of Smith-Waterman algorithm
  */
-void SW_bottomUp(char* X, char* Y, char** P, int** H, int n, int m){
+void SW_bottomUp(char *X, char *Y, char **P, int **H, int n, int m)
+{
+  for (int i = 0; i <= n; i++)
+  {
+    H[i][0] = 0;
+    P[i][0] = 0;
+  }
 
+  for (int i = 0; i <= m; i++)
+  {
+    H[0][m] = 0;
+    P[0][m] = 0;
+  }
+
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= m; j++)
+    {
+      char xi = X[i - 1];
+      char yj = Y[j - 1];
+      int p1, p2, p3;
+
+      if (xi == yj)
+      { // same char
+        p1 = H[i - 1][j - 1] + 2;
+      }
+      else
+      { // not same char
+        p1 = H[i - 1][j - 1] - 1;
+      }
+
+      p2 = H[i - 1][j] - 1;
+      p3 = H[i][j - 1] - 1;
+
+      H[i][j] = max(p1, p2);
+      H[i][j] = max(H[i][j], p3);
+
+      if (H[i][j] == p1)
+      {
+        P[i][j] = 'd';
+      }
+      else
+      {
+        if (H[i][j] == p2)
+        {
+          P[i][j] = 'u';
+        }
+        else
+        {
+          P[i][j] = 'l';
+        }
+      }
+    }
+  }
+
+  return;
 }
 
 /*
  * Top-down with memoization implementation of Smith-Waterman algorithm
  */
-void memoized_SW(char* X, char* Y, char** P, int** H, int n, int m){
-	
+void memoized_SW(char *X, char *Y, char **P, int **H, int n, int m)
+{
+  // the smallest negative value = -min(n, m)
+  // so we can initialize P tables value as -min(n, m) - 1 (except first row and first column)
+  // if the P[x][y] value > -min(n, m) - 1, means it has been visited, return value
+
+  int smallest = min(n, m) * -1;
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= m; j++)
+    {
+      H[i][j] = smallest - 1;
+    }
+  }
+
+  for (int i = 0; i <= n; i++)
+  {
+    H[i][0] = 0;
+    P[i][0] = 0;
+  }
+
+  for (int i = 0; i <= m; i++)
+  {
+    H[0][m] = 0;
+    P[0][m] = 0;
+  }
+
+  memoized_SW_AUX(X, Y, P, H, n, m, smallest);
+
+  return;
 }
 
 /*
  * Auxilary recursive function of top-down with memoization implementation of Smith-Waterman algorithm
  */
-void memoized_SW_AUX(char* X, char* Y, char** P, int** H, int n, int m){
-	
+void memoized_SW_AUX(char *X, char *Y, char **P, int **H, int n, int m, int smallest)
+{
+  // base case
+  if (n < 1 || m < 1)
+  {
+    return;
+  }
+
+  // int smallest = min(n, m) * -1;
+  char xi = X[n - 1];
+  char yj = Y[m - 1];
+  int p1, p2, p3;
+
+  // if (H[n - 1][m - 1] == smallest - 1), means not visited yest, call recursion to visit it
+  if (xi == yj)
+  {
+    if (H[n - 1][m - 1] == smallest - 1)
+    {
+      memoized_SW_AUX(X, Y, P, H, n - 1, m - 1, smallest);
+    }
+    p1 = H[n - 1][m - 1] + 2;
+  }
+  else
+  {
+    if (H[n - 1][m - 1] == smallest - 1)
+    {
+      memoized_SW_AUX(X, Y, P, H, n - 1, m - 1, smallest);
+    }
+    p1 = H[n - 1][m - 1] - 1;
+  }
+
+  if (H[n - 1][m] == smallest - 1)
+  {
+    memoized_SW_AUX(X, Y, P, H, n - 1, m, smallest);
+  }
+  p2 = H[n - 1][m] - 1;
+
+  if (H[n][m - 1] == smallest - 1)
+  {
+    memoized_SW_AUX(X, Y, P, H, n, m - 1, smallest);
+  }
+  p3 = H[n][m - 1] - 1;
+
+  H[n][m] = max(p1, p2);
+  H[n][m] = max(H[n][m], p3);
+
+  if (H[n][m] == p1)
+  {
+    P[n][m] = 'd';
+  }
+  else
+  {
+    if (H[n][m] == p2)
+    {
+      P[n][m] = 'u';
+    }
+    else
+    {
+      P[n][m] = 'l';
+    }
+  }
 }
 
 /*
  * Print X'
  */
-void print_Seq_Align_X(char* X, char** P, int n, int m){
-	
+void print_Seq_Align_X(char *X, char **P, int n, int m)
+{
+  if (n < 1 || m < 1)
+  {
+    return;
+  }
+
+  char p = P[n][m];
+  char x = X[n - 1];
+  if (p == 'd')
+  {
+    print_Seq_Align_X(X, P, n - 1, m - 1);
+    cout << x;
+  }
+  else
+  {
+    if (p == 'l')
+    {
+      print_Seq_Align_X(X, P, n, m - 1);
+      cout << '-';
+    }
+    else
+    {
+      print_Seq_Align_X(X, P, n - 1, m);
+      cout << x;
+    }
+  }
 }
 
 /*
  * Print Y'
  */
-void print_Seq_Align_Y(char* Y, char** P, int n, int m){
-	
+void print_Seq_Align_Y(char *Y, char **P, int n, int m)
+{
+  if (n < 1 || m < 1)
+  {
+    return;
+  }
+  char p = P[n][m];
+  char y = Y[m - 1];
+
+  if (p == 'd')
+  {
+    print_Seq_Align_Y(Y, P, n - 1, m - 1);
+    cout << y;
+  }
+  else
+  {
+    if (p == 'u')
+    {
+      print_Seq_Align_Y(Y, P, n - 1, m);
+      cout << '-';
+    }
+    else
+    {
+      print_Seq_Align_Y(Y, P, n, m - 1);
+      cout << y;
+    }
+  }
 }
--- HW4/cs590_hw4_code/main.cpp
+++ HW4/cs590_hw4_code/main.cpp
@@ -5,13 +5,13 @@
 #include "timer.h"
 #include "random_generator.h"
 
-int main(int argc, char* argv[])
-{ 
+int main(int argc, char *argv[])
+{
 	random_generator rg;
 	timer t;
 	int n, m, algo;
-	int** H;
-	char** P;		// possible values: 'l' for left, 'u' for up and 'd' for diagonal up left
+	int **H;
+	char **P; // possible values: 'l' for left, 'u' for up and 'd' for diagonal up left
 
 	/*
 	 * read the main arguments
@@ -37,58 +37,95 @@ int main(int argc, char* argv[])
 		m = (m < 1) ? 1 : m;
 	}
 	else
-		m=n;
+		m = n;
 	if (argc > 3)
 		algo = atoi(argv[3]);
 	else
 		algo = 0;
 
-	char* X;
-	char* Y;
+	char *X;
+	char *Y;
 
-	//values used for question #4.
-	// n=m=10;
-	// X = "dcdcbacbbb";
-	// Y = "acdccabdbb";
+	// values used for question #4.
+	//  n=m=10;
+	//  X = "dcdcbacbbb";
+	//  Y = "acdccabdbb";
 
 	random_generator::seed();
 
 	X = rg.random_string_m(n, 4);
 	Y = rg.random_string_m(m, 4);
 
-	H = new int*[n+1];
-	P = new char*[n+1];
-	for(int i=0;i<=n;i++)
+		// Inputs for testing
+	// char a[11] = {'c', 'a', 'c', 'a', 'c', 'c', 'c', 'b', 'a', 'b', 0};
+	// char b[11] = {'b', 'c', 'c', 'a', 'd', 'a', 'a', 'd', 'c', 'c', 0};
+
+	// char a[8] = {'a', 'a', 'a', 'a', 'a', 'a', 'a', 0};
+	// char b[3] = {'b', 'b', 0};
+
+	// char a[9] = {'a', 'c', 'b', 'a', 'b', 'a', 'b', 'a', 0};
+	// char b[9] = {'a', 'b', 'a', 'b', 'a', 'b', 'd', 'a', 0};
+
+	// char a[11] = {'c', 'a', 'a', 'c', 'b', 'd', 'a', 'c', 'c', 'a', 0};
+	// char b[11] = {'b', 'c', 'c', 'b', 'c', 'd', 'c', 'c', 'b', 'a', 0};
+
+	// X = a;
+	// Y = b;
+
+	H = new int *[n + 1];
+	P = new char *[n + 1];
+	for (int i = 0; i <= n; i++)
 	{
-		H[i] = new int[m+1];
-		P[i] = new char[m+1];
+		H[i] = new int[m + 1];
+		P[i] = new char[m + 1];
 	}
 
-	if(algo==0)
+	if (algo == 0)
 		SW_bottomUp(X, Y, P, H, n, m);
-	else if(algo==1)
+	else if (algo == 1)
 		memoized_SW(X, Y, P, H, n, m);
 
 	cout << endl;
 
 	cout << "\nX = ";
-	for(int i=0;i<n;i++)
+	for (int i = 0; i < n; i++)
 		cout << X[i];
 
 	cout << "\nX'= ";
-	print_Seq_Align_X(X,P,n,m);
+	print_Seq_Align_X(X, P, n, m);
 
 	cout << "\nY'= ";
-	print_Seq_Align_Y(Y,P,n,m);
+	print_Seq_Align_Y(Y, P, n, m);
 
 	cout << "\nY = ";
-	for(int j=0;j<m;j++)
+	for (int j = 0; j < m; j++)
 		cout << Y[j];
 
 	cout << "\n-----------------";
 
-	cout <<"\nM(n,m) = " << H[n][m] << endl;
+	// Outputs for testing
+	// cout << endl;
+	// for (int i = 0; i <= n; i++)
+	// {
+	// 	for (int j = 0; j <= m; j++)
+	// 	{
+	// 		cout << H[i][j] << " ";
+	// 	}
+	// 	cout << endl;
+	// }
+
+	// cout << "\n-----------------";
+
+	// for (int i = 0; i <= n; i++)
+	// {
+	// 	for (int j = 0; j <= m; j++)
+	// 	{
+	// 		cout << P[i][j];
+	// 	}
+	// 	cout << endl;
+	// }
+
+	cout << "\nM(n,m) = " << H[n][m] << endl;
 
 	return 0;
 }
-

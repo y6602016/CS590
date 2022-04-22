8648d4f89ef5375838bc30acff72e8d33dac9827
洪啟睿 <y6602016@gmail.com>
2022-04-22 19:53:23 -0400

    dfs/bfs

--- HW5/cs590_hw5_code/graph.cpp
+++ HW5/cs590_hw5_code/graph.cpp
@@ -9,25 +9,30 @@ using namespace std;
 Graph::Graph(int nodes)
 {
     this->nodes = nodes;
-    this->matrix = new int*[nodes];
+    this->matrix = new int *[nodes];
 
-    for (int i = 0; i < nodes; ++i) {
+    for (int i = 0; i < nodes; ++i)
+    {
         (this->matrix)[i] = new int[nodes];
 
-        for (int j = 0; j < nodes; ++j) {
-            (this->matrix)[i][j] = NO_EDGE;
+        for (int j = 0; j < nodes; ++j)
+        {
+            (this->matrix)[i][j] = 0;
         }
     }
 }
 
-Graph::Graph(int **matrix, int nodes) {
+Graph::Graph(int **matrix, int nodes)
+{
     this->nodes = nodes;
-    this->matrix = new int*[nodes];
+    this->matrix = new int *[nodes];
 
-    for (int i = 0; i < nodes; ++i) {
+    for (int i = 0; i < nodes; ++i)
+    {
         (this->matrix)[i] = new int[nodes];
 
-        for (int j = 0; j < nodes; ++j) {
+        for (int j = 0; j < nodes; ++j)
+        {
             (this->matrix)[i][j] = matrix[i][j];
         }
     }
@@ -35,15 +40,17 @@ Graph::Graph(int **matrix, int nodes) {
 
 Graph::~Graph()
 {
-    for (int i = 0; i < this->nodes; ++i) {
-        delete[] (this->matrix)[i];
+    for (int i = 0; i < this->nodes; ++i)
+    {
+        delete[](this->matrix)[i];
     }
     delete[] this->matrix;
 }
 
 bool Graph::set_edge(int i, int j, int edge)
 {
-    if (i < this->nodes && j < this->nodes && i >= 0 && j >= 0) {
+    if (i < this->nodes && j < this->nodes && i >= 0 && j >= 0)
+    {
         (this->matrix)[i][j] = edge;
         return true;
     }
@@ -53,18 +60,61 @@ bool Graph::set_edge(int i, int j, int edge)
 
 void Graph::dfs()
 {
-
+    set<int> visited;
+    for (int i = 0; i < this->nodes; i++)
+    {
+        if (visited.find(i) == visited.end())
+        {
+            // if the node is not in visited, explore it by dfs
+            this->dfs(i, &visited);
+        }
+    }
 }
 void Graph::dfs(int start, set<int> *visited)
 {
-    
+    // add the node into visited set
+    visited->insert(start);
+    cout << start << " ";
+    // explore matrix to find next node
+    for (int j = 0; j < this->nodes; j++)
+    {
+        if (((this->matrix)[start][j] != 0) && (visited->find(j) == visited->end()))
+        {
+            this->dfs(j, visited);
+        }
+    }
 }
 void Graph::bfs(int start)
 {
-    
+    if (start > nodes - 1 || start < 0)
+    {
+        cout << "Not valid" << endl;
+        return;
+    }
+
+    set<int> visited;
+    this->bfs(start, &visited);
 }
 void Graph::bfs(int start, set<int> *visited)
 {
+    queue<int> queue;
+    queue.push(start);
+    visited->insert(start);
 
+    while (!queue.empty())
+    {
+        int curr_node = queue.front();
+        queue.pop();
+
+        cout << curr_node << " ";
+
+        for (int j = 0; j < this->nodes; j++)
+        {
+            if (((this->matrix)[curr_node][j] != 0) && (visited->find(j) == visited->end()))
+            {
+                queue.push(j);
+                visited->insert(j);
+            }
+        }
+    }
 }
-
